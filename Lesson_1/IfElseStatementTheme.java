1bc04705d3fbfdb58f8099def2f3f388564ef6e7
andrjushasan <andrey.v.dorokhov@gmail.com>
2023-06-17 13:08:55 +0300

    Поправил IfElseStatementTheme по замечаниям ментора. В задаче 9 полностью изменён алгоритм.

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -3,27 +3,27 @@ public class IfElseStatementTheme {
     public static void main(String[] args) {
         System.out.println("Задача 1 \"Перевод псевдокода на язык Java\"");
         int age = 34;
-        boolean maleGender = true;
-        double height = 1.82;
-        char nameLetterFirst = "Андрей".charAt(0);
         if (age > 20) {
             System.out.println("Возраст больше 20 лет.");
         } else {
             System.out.println("Возраст меньше 20 лет.");
         }
+        boolean maleGender = true;
         if (!maleGender) {
             System.out.println("Пол женский.");
         } else {
             System.out.println("Пол мужской.");
         }
+        double height = 1.82;
         if (height > 1.8) {
             System.out.println("Рост больше 1 метра 80 сантиметров.");
         } else {
             System.out.println("Рост меньше 1 метра 80 сантиметров.");
         }
-        if (nameLetterFirst == 'М') {
+        char capitalLetter = "Андрей".charAt(0);
+        if (capitalLetter == 'М') {
             System.out.println("Первая буква имени - М.");
-        } else if (nameLetterFirst == 'И') {
+        } else if (capitalLetter == 'И') {
             System.out.println("Первая буква имени - И.");
         } else {
             System.out.println("Первая буква имени - не М и не И.");
@@ -42,8 +42,8 @@ public class IfElseStatementTheme {
 
         System.out.println("\nЗадача 3 \"Проверка числа\"");
         int anyNumber = 10;
+        System.out.print("Исходное число " + anyNumber + " является");
         if (anyNumber != 0) {
-            System.out.print("Исходное число " + anyNumber + " является");
             if (anyNumber % 2 == 0) {
                 System.out.print(" чётным");
             } else {
@@ -54,6 +54,8 @@ public class IfElseStatementTheme {
             } else {
                 System.out.println(" положительным");
             }
+        } else {
+            System.out.println(" нулём");
         }
 
         System.out.println("\nЗадача 4 \"Поиск одинаковых цифр в числах\"");
@@ -61,13 +63,13 @@ public class IfElseStatementTheme {
         int srcNumber2 = 321;
         System.out.print("Исходные числа: " + srcNumber1 + " и " + srcNumber2);
         int hundreds1 = srcNumber1 / 100;
-        int tens1 = (srcNumber1 % 100) / 10;
+        int tens1 = (srcNumber1 / 10) % 10;
         int ones1 = srcNumber1 % 10;
         int hundreds2 = srcNumber2 / 100;
-        int tens2 = (srcNumber2 % 100) / 10;
+        int tens2 = (srcNumber2 / 10) % 10;
         int ones2 = srcNumber2 % 10;
-        if ((hundreds1 != hundreds2) & (tens1 != tens2) & (ones1 != ones2)) {
-            System.out.println(", равных цифр в них нет");
+        if ((hundreds1 != hundreds2) && (tens1 != tens2) && (ones1 != ones2)) {
+            System.out.print(", равных цифр в них нет");
         } else {
             if (ones1 == ones2) {
                 System.out.print(", \nодинаковая " + ones1 + " в разряде 1");
@@ -94,44 +96,36 @@ public class IfElseStatementTheme {
         }
 
         System.out.println("\nЗадача 6 \"Подсчет суммы вклада и начисленных банком %\"");
-        int vklad = 300_000;
-        int percent = 0;
-        int calcPercent = 0;
-        int totalSum = 0;
-        if (vklad < 100_000) {
-            percent = 5;
-        } else if (vklad <= 300_000) {
+        int deposit = 300_000;
+        int percent = 5;
+        if (deposit >= 100_000 && deposit <= 300_000) {
             percent = 7;
-        } else {
+        } else if (deposit > 300_000) {
             percent = 10;
         }
-        calcPercent = vklad * percent / 100;
-        totalSum = vklad + calcPercent;
-        System.out.println("Сумма вклада = " + vklad + 
+        int calcPercent = deposit * percent / 100;
+        int totalSum = deposit + calcPercent;
+        System.out.println("Сумма вклада = " + deposit + 
                 "\nНачисленный % = " + calcPercent + 
                 "\nИтоговая сумма с % = " + totalSum);
 
         System.out.println("\nЗадача 7 \"Определение оценки по предметам\"");
         int percentHistory = 59;
-        int percentProgramming = 91;
-        int markHistory = 0;
-        int markProgramming = 0;
-        if (percentHistory <= 60) {
-            markHistory = 2;
-        } else if (percentHistory <= 73) {
+        int markHistory = 2;
+        if (percentHistory > 60 && percentHistory <= 73) {
             markHistory = 3;
-        } else if (percentHistory <= 91) {
+        } else if (percentHistory > 73 && percentHistory <= 91) {
             markHistory = 4;
-        } else {
+        } else if (percentHistory > 91) {
             markHistory = 5;
         }
-        if (percentProgramming <= 60) {
-            markProgramming = 2;
-        } else if (percentProgramming <= 73) {
+        int percentProgramming = 91;
+        int markProgramming = 2;
+        if (percentProgramming > 60 && percentProgramming <= 73) {
             markProgramming = 3;
-        } else if (percentProgramming <= 91) {
+        } else if (percentProgramming > 73 && percentProgramming <= 91) {
             markProgramming = 4;
-        } else {
+        } else if (percentProgramming > 91) {
             markProgramming = 5;
         }
         System.out.println(markHistory + " - история\n" + markProgramming + " - программирование\n" + 
@@ -154,51 +148,49 @@ public class IfElseStatementTheme {
         int sumRequested = 567;
         int hundredsAvailable = 10;
         int tensAvailable = 5;
-        int onesAvailable = 5;
+        int onesAvailable = 50;
         int hundredsRequired = 0;
         int tensRequired = 0;
         int onesRequired = 0;
-        int sumProcessed = sumRequested;
         if (sumRequested > (hundredsAvailable * 100 + tensAvailable * 10 + onesAvailable)) {
             System.out.println("Не хватает банкнот для выдачи суммы");
         } else {
-            if (sumProcessed / 100 < hundredsAvailable) {
-                hundredsRequired = sumProcessed / 100;
+            int sumProcessed = sumRequested;
+            int tempRequired = sumProcessed / 100;
+            if (tempRequired <= hundredsAvailable) {
+                hundredsRequired = tempRequired;
+                sumProcessed -= tempRequired * 100;
                 hundredsAvailable -= hundredsRequired;
-                sumProcessed -= hundredsRequired * 100;
-                if (sumProcessed / 10 < tensAvailable) {
-                    tensRequired = sumProcessed / 10;
-                    tensAvailable -= tensRequired;
-                    onesRequired = sumProcessed - tensRequired * 10;
-                    onesAvailable -= onesRequired;
-                } else {/*if 10 bankn is NOT enough!!!!*/
-                    tensRequired = tensAvailable;
-                    tensAvailable -= tensRequired;
-                    sumProcessed -= tensRequired * 10;
-                    onesRequired = sumProcessed;
-                    onesAvailable -= onesRequired;
-                }
             } else {
                 hundredsRequired = hundredsAvailable;
-                hundredsAvailable -= hundredsRequired;
-                sumProcessed -= hundredsRequired * 100;
-                if (sumProcessed / 10 < tensAvailable) {
-                    tensRequired = sumProcessed / 10;
-                    tensAvailable -= tensRequired;
-                    onesRequired = sumProcessed - tensRequired * 10;
-                    onesAvailable -= onesRequired;
-                } else {
-                    tensRequired = tensAvailable;
-                    tensAvailable -= tensRequired;
-                    sumProcessed -= tensRequired * 10;
-                    onesRequired = sumProcessed;
-                    onesAvailable -= onesRequired;
-                }
+                sumProcessed -= hundredsAvailable * 100;
+                hundredsAvailable = 0;
             }
+            int sumWithdraw = hundredsRequired * 100;
+            tempRequired = sumProcessed / 10;
+            if (tempRequired <= tensAvailable) {
+                tensRequired = tempRequired;
+                sumProcessed -= tempRequired * 10;
+                tensAvailable -= tensRequired;
+            } else {
+                tensRequired = tensAvailable;
+                sumProcessed -= tensAvailable * 10;
+                tensAvailable = 0;
+            }
+            sumWithdraw += tensRequired * 10;
+            tempRequired = sumProcessed;
+            if (tempRequired <= onesAvailable) {
+                onesRequired = tempRequired;
+                onesAvailable -= onesRequired;
+            } else {
+                onesRequired = onesAvailable;
+                onesAvailable = 0;
+            }
+            sumWithdraw += onesRequired;
             System.out.printf("%s","Номиналы и количества");
             System.out.printf("%n%10s%10s%10s", "100 USD", "10 USD", "1 USD");
             System.out.printf("%n%10d%10d%10d", hundredsRequired, tensRequired, onesRequired);
-            System.out.printf("%n%s%d%s", "Выдаваемая сумма: ", sumRequested, " USD");
+            System.out.printf("%n%s%d%s", "Выдаваемая сумма: ", sumWithdraw, " USD");
         }
     }
 }
